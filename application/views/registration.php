6d621bd7c07dd20c034b7c68aaf9547c68e6f20d
aleksking <yongboang@gmail.com>
2015-05-26 05:23:12 +0800

    registr

--- application/views/registration.php
+++ application/views/registration.php
@@ -1,8 +1,19 @@
-<table border="0">
-    <th>Registration Fees: <span>WPA Philippines 2016</span></th>
-    <th>EARLY BIRD FEE:<span>Until 1 Nov 2015</span></th>
-    <th>ADVANCE FEE:<span>Nov 2015 - Feb 2016</span></th>
-    <th>ONSITE FEE:<span>Feb 2016 Onwards</span></th>
+<h1>WPA REGISTRATION RATES</h1>
+
+<span style="font-size:18px; color:#2d5c88;">Delegates, Students and other Professionals</span>
+<br>
+<p>Attending all Congress Scientific Sessions<br>
+  Participation in Opening Ceremony, Welcome Cocktail and Closing Ceremonies<br>
+  Admission to Exhibit Area<br>
+  Congress bag, Congress Documents, Abstract, Final Programme and other congress materials<br>
+  Coffee Breaks
+</p>
+
+<table border="0" class="rate_list">
+    <th>REGISTRATION FEES <br><span>WPA Philippines 2016</span></th>
+    <th>EARLY BIRD FEE <br><span>Until Nov 2015</span></th>
+    <th>ADVANCE FEE <br><span>Nov 2015 - Feb 2016</span></th>
+    <th>ONSITE FEE <br><span>Feb 2016 Onwards</span></th>
   <tr>
     <td>Psychiatrist/Physician from Group A Country</td>
     <td>P26,100.00 / USD 580</td>
@@ -65,3 +76,94 @@
   </tr>
 
 </table>
+
+<p>* Please refer to the list of countries by groups below<br>
+* Current school ID is required to validate your status.</p>
+
+<span style="font-size:18px; color:#2d5c88;">Group A : Upper and High Income Economies by world Bank</span>
+<br>
+
+
+<div style="clear:both"></div>
+<div class="box_highlight" style="">
+  <div class="one-fourth">
+    <p>Andorra<br>
+Aruba<br>
+Australia<br>
+Austria<br>
+Bahamas, The<br>
+Bahrain<br>
+Barbados<br>
+Belgium<br>
+Bermuda<br>
+Brunei Darussalam<br>
+Canada<br>
+Cayman Islands<br>
+Channel Islands<br>
+Croatia<br>
+Curacao<br>
+Cyprus<br></p>
+
+  </div>
+
+  <div class="one-fourth">
+  <p>Denmark<br>
+Estonia<br>
+Faeroe Islands<br>
+Finland<br>
+France<br>
+French Polynesia<br>
+Germany<br>
+Greece<br>
+Greenland<br>
+Guam<br>
+Hongkong SAR, China<br>
+Hungary<br>
+Iceland<br>
+Ireland<br>
+Isle of Man<br>
+Israel<br>
+Italy<br>
+</p>
+  </div>
+
+  <div class="one-fourth">
+  <p>Japan<br>
+Korea, Rep<br>
+Kuwait<br>
+Liechtenstein<br>
+Luxembourg<br>
+Malta<br>
+Monaco<br>
+New Celedonia<br>
+New Zealand<br>
+Northern Mariana Islands<br>
+Norway<br>
+Oman<br>
+Poland<br>
+Portugal<br>
+Puerto Rico<br>
+Qatar<br>
+San Marino<br>
+</p>
+  </div>
+
+  <div class="one-fourth last">
+  <p>Saudi Arabia<br>
+Singapore<br>
+Sint Maarten<br>
+Slovak Republic<br>
+Slovenia<br>
+Spain<br>
+St Kitts and Nevis<br>
+St Martin<br>
+Sweden<br>
+Switzerland<br>
+Trinidad and Tobago<br>
+Turks and Caicos Islands<br>
+United Kingdom<br>
+Untied States<br>
+Virgin Islands (US)<br>
+</p>
+  </div>
+</div>
