8d97004fa8aa2db7d1f323884e4efb7f58a7103d
aleksking <yongboang@gmail.com>
2015-05-27 06:12:36 +0800

    morning changes

--- application/views/index.php
+++ application/views/index.php
@@ -45,49 +45,9 @@
 	</p>
 </div>
 
-<div class="one-third last box_highlight" style="width: 280px; clear: none;float: right;margin: 0 0 0 20px;padding: 20px;">
+<div class="one-third last box_highlight" style="width: 280px; clear: none;float: right;margin: 0 0 0 20px;padding: 20px;background: #e8e8e8;">
 	<h2 style="font-size: 17px; margin: 10px 0 20px;"><a href="<?php echo base_url(); ?>about_philippines">It's More Fun in the Philippines</a></h2>
-	<a href="<?php echo base_url(); ?>about_philippines"><img src="<?php echo base_url(); ?>img/wpa/mag/sunset.png" alt="It's More Fun in the Philippines" /></a>
+	<a href="<?php echo base_url(); ?>about_philippines"><img src="<?php echo base_url(); ?>img/wpa/mag/sm_mall_of_asia.png" alt="It's More Fun in the Philippines" /></a>
 	<p>The Philippines is an archipelagic Democratic state that is a melting pot of Eastern and Western cultures. Prior to being colonized by the Spaniards in 1565, it was already populated by a diverse society that traded with neighbouring cultures.</p>
 	<p>The arrival of the Spaniards, when Ferdinand Magellan landed in Samar in 1521 signalled a turning point in Philippine society. The country, named after King Felipe II, came together under a unified political and religious structure, with the introduction of Catholicism, as well as the code of law. The friars, who were the main instrument of colonisation...  <a href="<?php echo base_url(); ?>about_philippines" class="button_small white">Continue Reading &raquo;</a></p>
 </div>
-
-
-<!-- <div class="one-third box_highlight" style="width: 280px; padding: 10px; clear: none; margin: 10px 27px 0 0;">
-	<h2 style="font-size: 17px; margin: 10px 0 20px;">It's More Fun in the Philippines</h2>
-	<img src="<?php echo base_url(); ?>img/wpa/mag/sunset.png" alt="image description" />
-	<p>The Philippines is an archipelagic Democratic state that is a melting pot of Eastern and Western cultures. Prior to being colonized by the Spaniards in 1565, it was already populated by a diverse...  <a href="<?php echo base_url(); ?>about_philippines" class="button_small white">Continue Reading &raquo;</a></p>
-</div>
-
-<div class="one-third box_highlight" style="width: 280px; padding: 10px; clear: none; margin: 10px 27px 0 0;">
-	<h2 style="font-size: 17px; margin: 10px 0 20px;">Language</h2>
-	<img src="<?php echo base_url(); ?>img/wpa/mag/sunset.png" alt="image description" />
-	<p>Filipino, which is a variation of the Tagalog dialect, is the National Language. English is widely spoken. Waylay an ordinary pedestrian and you can hold a decent conversation with him, albeit...  <a href="<?php echo base_url(); ?>language" class="button_small white">Continue Reading &raquo;</a></p>
-</div>
-
-<div class="one-third last box_highlight" style="width: 280px; padding: 10px; clear: none; margin: 10px 0 0 0;">
-	<h2 style="font-size: 17px; margin: 10px 0 20px;">Dining and Shopping</h2>
-	<img src="<?php echo base_url(); ?>img/wpa/mag/sunset.png" alt="image description" />
-	<p>There are notable destinations near the Philippine International Convention Centre for dining, most of them posh hotels, among them the historic Manila Hotel, Bayleaf Hotel, Hotel Sofitel, and Manila...  <a href="<?php echo base_url(); ?>dining_shopping" class="button_small white">Continue Reading &raquo;</a></p>
-</div>
-
-<div class="one-third box_highlight" style="width: 280px; padding: 10px; clear: none; margin: 30px 27px 0 0;">
-	<h2 style="font-size: 17px; margin: 10px 0 20px;">Historical Sites</h2>
-	<img src="<?php echo base_url(); ?>img/wpa/mag/sunset.png" alt="image description" />
-	<p>The Convention Venue is near relics of Spanish rule such as the walled city of Intramuros, and within it, the Fort Santiago, where the National Hero Gat. Jose P.Rizal was incarcerated prior to his execution...  <a href="<?php echo base_url(); ?>historical_sites" class="button_small white">Continue Reading &raquo;</a></p>
-</div>
-
-<div class="one-third box_highlight" style="width: 280px; padding: 10px; clear: none; margin: 30px 27px 0 0;">
-	<h2 style="font-size: 17px; margin: 10px 0 20px;">Sunset by the Bay</h2>
-	<img src="<?php echo base_url(); ?>img/wpa/mag/sunset.png" alt="Manila Bay" />
-	<p>Most shops accept credit cards, but bazaars and public markets don't so it's best to carry cash with you. Carry them in small denominations (hundreds and below, no five hundred and one thousand notes) and...  <a href="<?php echo base_url(); ?>credit_card" class="button_small white">Continue Reading &raquo;</a></p>
-</div>
-
-<div class="one-third last box_highlight" style="width: 280px; padding: 10px; clear: none; margin: 30px 0 0 0;">
-	<h2 style="font-size: 17px; margin: 10px 0 20px;">Credit Cards</h2>
-	<img src="<?php echo base_url(); ?>img/wpa/mag/sunset.png" alt="image description" />
-	<p>The Philippines is an archipelagic Democratic state that is a melting pot of Eastern and Western cultures. Prior to being colonized by the Spaniards in 1565, it was already populated by a diverse...  <a href="<?php echo base_url(); ?>about_philippines" class="button_small white">Continue Reading &raquo;</a></p>
-</div> -->
-
-
-
